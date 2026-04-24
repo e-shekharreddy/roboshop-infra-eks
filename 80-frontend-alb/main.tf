bcedd6be2e10602f691f2b5d68b1bfdfa4bdffc4
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-04-24 09:34:59 +0530

    k8-infra-eks

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -86,3 +86,54 @@ resource "aws_route53_record" "www" {
   }
   allow_overwrite = true
 }
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "${var.project}-${var.environment}-frontend"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 10
+    path = "/"
+    port = 80
+    matcher = "200-299"
+    protocol = "HTTP"
+    timeout = 5
+
+  }
+  
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.project}-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.${var.domain_name}"
+  type    = "A"
+  
+  # load balancer details
+  alias {
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+    evaluate_target_health = true
+  }
+  allow_overwrite = true
+}
