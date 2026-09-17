2bc39ab2d045b6dbe20fd4c2dd9321e3295a5ab7
e-shekharreddy <shekharreddy.eanala.com>
2026-09-17 12:10:14 +0530

    python basics

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -170,15 +170,6 @@ resource "aws_security_group_rule" "jenkins_ssh" {
   security_group_id = local.jenkins_sg_id
 }
 
-/* resource "aws_security_group_rule" "jenkins_agent_jenkins" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp" # all traffic
-  # VPC CIDR
-  source_security_group_id = local.jenkins_sg_id
-  security_group_id = local.jenkins_agent_sg_id
-} */
 
 resource "aws_security_group_rule" "jenkins_agent_ssh" {
   type              = "ingress"
